35e3cde87f61870c34c64577d5aa94ef6334bb7e
Anish Sanghvi <z5421465@ad.unsw.edu.au>
2025-02-06 21:39:34 +1100

    adding tailwind and added bg video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+// Import modules
+import { useEffect, useState } from 'react';
+
 // Import Components
 import Landing from './components/Landing.jsx';
 
+// Import App.css for tailwind
+import './App.css';
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <>
-      <Landing />
+      <Landing isLoading={isLoading} setIsLoading={setIsLoading}/>
     </>
   )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,12 +1,41 @@
 import '../styles/Landing.css';
 import wavesBg from '../assets/wavesBackground.mp4';
+import { useEffect, useState } from 'react';
 
-export default function Landing() {
+export default function Landing({isLoading, setIsLoading}) {
+
+    const handleVideoLoaded = () => {
+        const interval = setInterval(() => {
+            setIsLoading(false);
+        }, 1000);
+        
+        return () => {
+            clearInterval(interval)
+        }
+    };
 
     return (
         <>
-        <div>
-            Hi
+        <div id='landingContainer' className='flex w-[100vw] h-[100vh] items-center justify-center text-white p-[0]'>
+            {/* Background overlay */}
+            <div className="absolute inset-0 bg-[#111e21] z-[-11]"></div>
+
+            {/* Video Background */}
+            <video
+                id="backgroundWaves"
+                className={`${isLoading ? 'opacity-0' : 'opacity-100'} w-[100vw] h-[100vh] fixed top-0 left-0 w-screen h-screen object-cover -z-10 brightness-50 overflow-hidden overflow-hidden transition-opacity duration-6000 ease-in-out`}
+                autoPlay
+                loop
+                muted
+                onCanPlayThrough={handleVideoLoaded}
+            >
+                <source src={wavesBg} type="video/mp4" />
+            </video>
+
+            {/* Navbar */}
+            <h1 className='text-6xl'>Hi</h1>
+
+            {/* Text */}
         </div>
         </>
     )
