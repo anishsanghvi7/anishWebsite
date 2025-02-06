30acb018048810ac54be847dfdf90f72c4e817c2
Anish Sanghvi <z5421465@ad.unsw.edu.au>
2025-02-06 18:14:25 +1100

    setting up directory

---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import '../styles/Landing.css';
+import wavesBg from '../assets/wavesBackground.mp4';
+
+export default function Landing() {
+
+    return (
+        <>
+        <div>
+            Hi
+        </div>
+        </>
+    )
+}
